4b7145127bd23c16c0dee97e3d6b0ca21194f901
Dmitry <DvoroninD@yandex.ru>
2022-06-07 15:27:08 +0300

    scrolling improved

--- scroll.js
+++ scroll.js
@@ -14,8 +14,9 @@
     }
 
     function backToTop() {
+        var scrollStep = window.pageYOffset / 40;
         if (window.pageYOffset > 0) {
-            window.scrollBy(0, -20);
+            window.scrollBy(0, -(scrollStep));
             setTimeout(backToTop, 0);
         }
     }
